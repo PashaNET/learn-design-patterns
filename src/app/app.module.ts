ee620eb21c26bc8e52ad1be6325acff73d8f9432
PashaNET <pavlo.netrebsky@gmail.com>
2018-01-31 11:43:04 +0200

    DragNDrop example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ export class DemoMaterialModule {}
     AppRoutingModule,
     DemoMaterialModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [ 
     PatternService, 
--- src/app/drag-n-drop/drag-n-drop.component.ts
+++ src/app/drag-n-drop/drag-n-drop.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drag-n-drop.component.styl']
 })
 export class DragNDropComponent implements OnInit {
+  isDragStarted: boolean = false;
+  topPos: number = 0;
+  leftPos: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  dragStart(event: any) {
+    console.log(event);
+    this.topPos = event.pageY;
+    this.leftPos = event.pageX;
+    this.isDragStarted = !this.isDragStarted;
+  }
+
+  dragMove(event: any) {
+    this.topPos = event.pageY;
+    this.leftPos = event.pageX;
+  }
+
+  dragEnd(event: any) {
+    console.log('end');
+    this.isDragStarted = !this.isDragStarted;
+  }
+//set absolute
+//take mouse positions
+//set position to element
 }
